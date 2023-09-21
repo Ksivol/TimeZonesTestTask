550171628118574223b2d6efb7715b515c21543f
Ksivol <surkin-ant@mail.ru>
2023-09-21 21:03:19 +0400

    Initial commit

---
+++ app/src/main/java/com/ksivol_project/timezonestesttask/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ksivol_project.timezonestesttask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
